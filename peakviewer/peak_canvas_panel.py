362fef8551f969191754b1b4f2bf8b9f26e5f58e
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2023-08-10 14:08:41 +0100

    Rescale y axis when zooming or panning x axis and use proper scientific notation

--- peakviewer/peak_canvas_panel.py
+++ peakviewer/peak_canvas_panel.py
@@ -25,30 +25,41 @@ wxPython panel for displaying peaks using matplotlib.
 #  OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
 #  OR OTHER DEALINGS IN THE SOFTWARE.
 #
+# y-axis rescaling (including calculate_new_limit) from
+# https://stackoverflow.com/questions/29461608/fixing-x-axis-scale-and-autoscale-y-axis
+# By Daniel H. (https://stackoverflow.com/users/8006684/daniel-h)
+# and TomNorway (https://stackoverflow.com/users/1018861/tomnorway)
+# CC BY-SA 4.0
+#
 
 # stdlib
+import math
 import sys
-from typing import List, Optional
+from typing import TYPE_CHECKING, List, Optional, Tuple
 
 # 3rd party
-import matplotlib  # type: ignore[import]
+import matplotlib
 import wx  # type: ignore[import]
 from libgunshotmatch.project import Project
-from matplotlib.axes import Axes  # type: ignore[import]
-from matplotlib.backend_bases import MouseButton, MouseEvent  # type: ignore[import]
-from matplotlib.figure import Figure  # type: ignore[import]
+from matplotlib.axes import Axes
+from matplotlib.backend_bases import Event, MouseButton
+from matplotlib.figure import Figure
+from matplotlib.lines import Line2D
 
 matplotlib.use("WXAgg")
 
 # 3rd party
-from matplotlib.backends.backend_wxagg import (  # type: ignore[import]  # noqa: E402
-		FigureCanvasWxAgg as FigureCanvas)
-from matplotlib.backends.backend_wxagg import NavigationToolbar2WxAgg as NavigationToolbar2Wx  # noqa: E402
+from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg  # noqa: E402
+from matplotlib.backends.backend_wxagg import NavigationToolbar2WxAgg  # noqa: E402
 
-__all__ = ["PeakCanvasPanel", "PeakCanvasToolbar", "XPanAxes"]
+if TYPE_CHECKING:
+	# 3rd party
+	import numpy
+
+__all__ = ("PeakCanvasPanel", "PeakCanvasToolbar", "XPanAxes")
 
 
-class XPanAxes(matplotlib.axes.Axes):
+class XPanAxes(Axes):
 	"""
 	Constrain pan to x-axis.
 	"""
@@ -57,9 +68,12 @@ class XPanAxes(matplotlib.axes.Axes):
 
 	def drag_pan(self, button: MouseButton, key: Optional[str], x: float, y: float) -> None:
 		"""
+		Handler for the pan action.
+
+		Constrains pan to the x (time) axis.
 
 		:param button: The pressed mouse button.
-		:param key: The pressed key, if any.
+		:param key: The pressed key, if any. Ignored and always substituted with the ``x`` key.
 		:param x: The mouse coordinates in display coords.
 		:param y: The mouse coordinates in display coords.
 		"""
@@ -67,16 +81,32 @@ class XPanAxes(matplotlib.axes.Axes):
 		# pretend key=='x'
 		Axes.drag_pan(self, button, 'x', x, y)
 
+	def format_ydata(self, y: float) -> str:
+		"""
+		Format a y-axis value in scientific notation.
+		"""
+
+		return _format_scientific(y)
+
+	def format_coord(self, x: float, y: float) -> str:
+		"""
+		Format the x and y coordinates.
+		"""
+
+		formatted_x = "???" if x is None else self.format_xdata(x)
+		formatted_y = "???" if y is None else self.format_ydata(y)
+		return f"T={formatted_x} mins y={formatted_y}     ​"
+
 
 matplotlib.projections.register_projection(XPanAxes)
 
 
-class PeakCanvasToolbar(NavigationToolbar2Wx):
+class PeakCanvasToolbar(NavigationToolbar2WxAgg):
 	"""
 	Modified Matplotlib toolbar for the peak canvas.
 	"""
 
-	_zoom_info: "matplotlib.backend_bases._ZoomInfo"
+	_zoom_info: "matplotlib.backend_bases._ZoomInfo"  # type: ignore[name-defined]
 
 	toolitems = (
 			("Home", "Reset original view", "home", "home"),
@@ -85,22 +115,56 @@ class PeakCanvasToolbar(NavigationToolbar2Wx):
 			(None, None, None, None),
 			("Pan", "Pan along x axis, Right button zooms", "move", "pan"),
 			("Zoom", "Zoom along x axis", "zoom_to_rect", "zoom"),
-			# ('Subplots', 'Configure subplots', 'subplots', 'configure_subplots'),
+			("Subplots", "Configure subplots", "subplots", "configure_subplots"),
 			# (None, None, None, None),
 			# ('Save', 'Save the figure', 'filesave', 'save_figure'),
 			# TODO: override to temporarily add peak info to title
 			)
 
-	def drag_zoom(self, event: MouseEvent) -> None:
+	def drag_zoom(self, event: Event) -> None:
 		"""
 		Handler for the zoom action.
 
 		Constrains zoom to the x (time) axis.
 		"""
 
+		# print(self._zoom_info)
 		# print(self._zoom_info.cbar)
 		self._zoom_info = self._zoom_info._replace(cbar="horizontal")
-		return super().drag_zoom(event)
+		ret = super().drag_zoom(event)
+		return ret
+
+	def set_message(self, s: str) -> None:
+		"""
+		Set the status message (mouse coordinates) in the taskbar.
+
+		:param s: The message string.
+		"""
+
+		if self._coordinates:
+			self._label_text.SetLabel(s)
+
+
+def calculate_new_limit(fixed: "numpy.ndarray", dependent: "numpy.ndarray", limit: Tuple[float, float]) -> float:
+	"""
+	Calculates the min/max of the dependent axis given a fixed axis with limits.
+
+	:param fixed:
+	:param dependent:
+	:param limit:
+	"""
+
+	if len(fixed) > 2:
+		mask = (fixed > limit[0]) & (fixed < limit[1])
+		window = dependent[mask]
+		high = window.max()
+
+	else:
+		high = dependent[-1]
+		if dependent[0] == 0.0 and high == 1.0:
+			# This is a axhline in the autoscale direction
+			high = 0
+	return high
 
 
 class PeakCanvasPanel(wx.Panel):
@@ -114,10 +178,12 @@ class PeakCanvasPanel(wx.Panel):
 		wx.Panel.__init__(self, parent)
 
 		self.figure = Figure()
-		self.axes = []
+		self.axes: List[Axes] = []
 		self.reset(n_repeats)
 
-		self.canvas = FigureCanvas(self, -1, self.figure)
+		self.figure.canvas.mpl_connect("draw_event", self.rescale_y_axis)
+
+		self.canvas = FigureCanvasWxAgg(self, -1, self.figure)
 
 		self.toolbar = PeakCanvasToolbar(self.canvas)
 		self.toolbar.Realize()
@@ -146,14 +212,54 @@ class PeakCanvasPanel(wx.Panel):
 				)
 		self.rejected_peak_stamp.set_alpha(0)
 
+	def rescale_y_axis(self, event: Event) -> None:
+		"""
+		Rescale the y-axis in response to a change in the x axis limits (pan or zoom).
+		"""
+
+		# Only redraw if there needs to be a change,
+		# otherwise there'll be an infinite loop.
+		should_redraw = False
+
+		# ax: Axes
+		for ax in self.axes:
+			for artist in ax.lines:
+				assert isinstance(artist, Line2D)
+				x: "numpy.ndarray" = artist.get_xdata()  # type: ignore[assignment]
+				y: "numpy.ndarray" = artist.get_ydata()  # type: ignore[assignment]
+				try:
+					high = calculate_new_limit(x, y, ax.get_xlim())
+				except Exception:
+					continue
+
+				newhigh = high if high > 0 else 0
+				current_ylim = ax.get_ylim()[1]
+				new_ylim = newhigh * 1.2  # Add a little space above the line
+				# print(new_ylim, current_ylim)
+
+				if new_ylim != current_ylim:
+					ax.set_ylim(0, new_ylim)
+					should_redraw = True
+
+		if should_redraw:
+			self.refresh()
+
 	def reset(self, n_repeats: int) -> None:
 		"""
 		Create ``n_repeats`` new, empty axes.
+
+		:param n_repeats:
 		"""
 
 		for ax in self.axes:
 			ax.remove()
-		self.axes = self.figure.subplots(n_repeats, 1, sharex=True, subplot_kw=dict(projection="XPanAxes"))
+
+		self.axes = self.figure.subplots(  # type: ignore[assignment]
+			n_repeats,
+			1,
+			sharex=True,
+			subplot_kw=dict(projection="XPanAxes"),
+			)
 
 	def do_layout(self) -> None:
 		"""
@@ -246,21 +352,36 @@ class PeakCanvasPanel(wx.Panel):
 					self.axes[repeat_idx].get_ylim()[1] * 0.2,
 					f" {peak.rt/60:0.3f}",
 					)
-
 		self.figure.supylabel("Intensity")
 		self.axes[0].autoscale()
 		self.axes[-1].set_xlabel("Retention Time (mins)")
 		for ax, repeat_name in zip(self.axes, project.datafile_data):
+			ax.ticklabel_format(axis='y', scilimits=(0, 0), useMathText=True)
 			ax.set_ylim(bottom=0)
 			# xmin, xmax = ax.get_xlim()
 			# ax.text(xmin + (xmax-xmin)*0.05, ax.get_ylim()[1] *0.8, repeat_name)
 			ax.annotate(repeat_name, (0.01, 0.8), xycoords="axes fraction")
 		self.axes[0].set_xlim(min_rt / 60, max_rt / 60)
 		# self.figure.subplots_adjust(bottom=0, top=1, left=0, right=1, hspace=0, wspace=0)
-		self.figure.subplots_adjust(top=0.95, right=0.95)
+		# self.figure.subplots_adjust(top=0.95, right=0.95)
+		self.figure.subplots_adjust(bottom=0.1, left=0.1, top=0.95, right=0.98, hspace=0.3)
 		self.refresh()
 
 		# self.Layout()
 		# self.Update()
 		# self.Layout()
 		# self.Update()
+
+
+_superscript_translate_map = {ord(str(num)): sup for num, sup in enumerate("⁰¹²³⁴⁵⁶⁷⁸⁹")}
+_superscript_translate_map[ord('-')] = '⁻'
+print(_superscript_translate_map)
+
+
+def _format_scientific(value: float) -> str:
+	n = math.floor(math.log10(value))
+	significand = value / 10**n
+	if n <= 1:
+		return f"{value:0.3f}"
+	power = str(n).translate(_superscript_translate_map)
+	return f'{significand:0.3f}×10{power}'
